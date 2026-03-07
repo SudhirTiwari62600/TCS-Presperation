3711885dabfe49ec87fad0a4eb9ae7b05b03d6af
code-with-innovater <st168035@gmail.com>
2026-03-07 22:21:46 +0530

    check even number or odd number

--- Printeven.java
+++ Printeven.java
@@ -3,10 +3,10 @@ public class Printeven {
     public static void main (String[] args){
         Scanner sc = new Scanner (System.in);
         int n = sc.nextInt();
-        for(int i =0;i<=n;i++){
-            if(i%2==0){
-                System.out.println(i);
-            }
+        if(n%2==0){
+            System.out.println("Even number");
+        }else{
+            System.out.println("Odd number");
         }
     }
 }
