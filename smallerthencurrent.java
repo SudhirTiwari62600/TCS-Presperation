62d2ec34bc98d23fac13fd1d509a7e00364c9771
code-with-innovater <st168035@gmail.com>
2026-03-09 22:01:29 +0530

    smaller number then current

---
+++ smallerthencurrent.java
@@ -0,0 +1,26 @@
+import java.util.Arrays;
+public class smallerthencurrent {
+  public static int[] smallerNumbersThanCurrent(int[] nums) {
+   int n = nums.length;
+        int[] result = new int[n];
+
+        for(int i = 0; i < n; i++) {
+            int count = 0;
+
+            for(int j = 0; j < n; j++) {
+                if(nums[j] < nums[i] && i != j) {
+                    count++;
+                }
+            }
+
+            result[i] = count;
+        }
+
+        return result;
+    }
+
+    public static void main(String[] args) {
+        int nums[] = {8,1,2,2,3};
+        System.out.println(Arrays.toString(smallerNumbersThanCurrent(nums)));
+    }
+}
